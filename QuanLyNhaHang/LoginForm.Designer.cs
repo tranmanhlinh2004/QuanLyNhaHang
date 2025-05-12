f0187a8f161d0e50a764fc89b54652155fc4f32e
TranManhLinh <tranmanhlinh204@gmail.com>
2025-05-12 12:49:12 +0700

    tao form dang ky

--- QuanLyNhaHang/LoginForm.Designer.cs
+++ QuanLyNhaHang/LoginForm.Designer.cs
@@ -34,52 +34,52 @@
             txtMatkhau = new TextBox();
             btnLogin = new Button();
             btnClose = new Button();
+            label3 = new Label();
+            linkLabel1 = new LinkLabel();
+            label4 = new Label();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 14F);
-            label1.Location = new Point(75, 88);
+            label1.Location = new Point(153, 123);
             label1.Name = "label1";
-            label1.Size = new Size(174, 32);
+            label1.Size = new Size(141, 25);
             label1.TabIndex = 0;
-            label1.Text = "Tên đăng nhập";
+            label1.Text = "Tên đăng nhập:";
             // 
             // label2
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 14F);
-            label2.Location = new Point(75, 163);
+            label2.Location = new Point(153, 179);
             label2.Name = "label2";
-            label2.Size = new Size(115, 32);
+            label2.Size = new Size(95, 25);
             label2.TabIndex = 0;
-            label2.Text = "Mật khẩu";
+            label2.Text = "Mật khẩu:";
             // 
             // txtTendangnhap
             // 
-            txtTendangnhap.Location = new Point(251, 91);
-            txtTendangnhap.Margin = new Padding(3, 4, 3, 4);
+            txtTendangnhap.Location = new Point(307, 125);
             txtTendangnhap.Name = "txtTendangnhap";
-            txtTendangnhap.Size = new Size(193, 27);
+            txtTendangnhap.Size = new Size(213, 23);
             txtTendangnhap.TabIndex = 1;
             // 
             // txtMatkhau
             // 
-            txtMatkhau.Location = new Point(251, 165);
-            txtMatkhau.Margin = new Padding(3, 4, 3, 4);
+            txtMatkhau.Location = new Point(307, 181);
             txtMatkhau.Name = "txtMatkhau";
             txtMatkhau.PasswordChar = '●';
-            txtMatkhau.Size = new Size(193, 27);
+            txtMatkhau.Size = new Size(213, 23);
             txtMatkhau.TabIndex = 2;
             // 
             // btnLogin
             // 
             btnLogin.Font = new Font("Segoe UI", 12F);
-            btnLogin.Location = new Point(106, 277);
-            btnLogin.Margin = new Padding(3, 4, 3, 4);
+            btnLogin.Location = new Point(180, 274);
             btnLogin.Name = "btnLogin";
-            btnLogin.Size = new Size(142, 57);
+            btnLogin.Size = new Size(124, 43);
             btnLogin.TabIndex = 2;
             btnLogin.Text = "Đăng nhập";
             btnLogin.UseVisualStyleBackColor = true;
@@ -88,27 +88,61 @@
             // btnClose
             // 
             btnClose.Font = new Font("Segoe UI", 12F);
-            btnClose.Location = new Point(384, 277);
-            btnClose.Margin = new Padding(3, 4, 3, 4);
+            btnClose.Location = new Point(370, 274);
             btnClose.Name = "btnClose";
-            btnClose.Size = new Size(142, 57);
+            btnClose.Size = new Size(124, 43);
             btnClose.TabIndex = 2;
             btnClose.Text = "Thoát";
             btnClose.UseVisualStyleBackColor = true;
             btnClose.Click += btnClose_Click;
             // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Cambria", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(255, 56);
+            label3.Name = "label3";
+            label3.Size = new Size(174, 37);
+            label3.TabIndex = 0;
+            label3.Text = "Đăng nhập";
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Font = new Font("Segoe UI", 11F);
+            linkLabel1.LinkColor = Color.Black;
+            linkLabel1.Location = new Point(447, 221);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(63, 20);
+            linkLabel1.TabIndex = 15;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Đăng ký";
+            linkLabel1.LinkClicked += linkLabel1_LinkClicked;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 11F);
+            label4.Location = new Point(306, 221);
+            label4.Name = "label4";
+            label4.Size = new Size(135, 20);
+            label4.TabIndex = 14;
+            label4.Text = "Chưa có tài khoản?";
+            // 
             // LoginForm
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(688, 376);
+            ClientSize = new Size(678, 368);
+            Controls.Add(linkLabel1);
+            Controls.Add(label4);
             Controls.Add(btnClose);
             Controls.Add(btnLogin);
             Controls.Add(txtMatkhau);
             Controls.Add(txtTendangnhap);
+            Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
-            Margin = new Padding(3, 4, 3, 4);
             Name = "LoginForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "LoginForm";
@@ -125,5 +159,8 @@
         private TextBox txtMatkhau;
         private Button btnLogin;
         private Button btnClose;
+        private Label label3;
+        private LinkLabel linkLabel1;
+        private Label label4;
     }
 }
---
+++ QuanLyNhaHang/SignUpForm.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Xml.Linq;
+
+namespace QuanLyNhaHang
+{
+    public partial class SignUpForm : Form
+    {
+        private string conStr = "Data Source=(local);Initial Catalog=QuanLyNhaHang;Integrated Security=True";
+        private SqlConnection mySqlConnection;
+        private SqlCommand mySqlCommand;
+        public SignUpForm()
+        {
+            InitializeComponent();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            LoginForm loginForm = new LoginForm();
+            loginForm.Show();
+            this.Hide();
+        }
+
+        private void SignUpForm_Load(object sender, EventArgs e)
+        {
+            mySqlConnection = new SqlConnection(conStr);
+            mySqlConnection.Open();
+
+        }
+
+        private void btnSignUp_Click(object sender, EventArgs e)
+        {
+            string name = txtTendangky.Text;
+            string password = txtMatkhaudangky.Text;
+            string confirmPassword = txtXacnhanmatkhau.Text;
+            string check;
+
+            if (name == "" || password == "" || confirmPassword == "")
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            if (password != confirmPassword)
+            {
+                MessageBox.Show("Mật khâu xác nhận không khớp!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            check = "SELECT * FROM DangNhap WHERE Ten_dang_nhap = N'" + name + "'";
+            mySqlCommand = new SqlCommand(check, mySqlConnection);
+            SqlDataReader drCheck = mySqlCommand.ExecuteReader();
+
+            if (drCheck.HasRows == true)
+            {
+                MessageBox.Show("Tên đăng nhập đã tồn tại!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtTendangky.Focus();
+                txtTendangky.Clear();
+                drCheck.Close();
+                return;
+            }
+            drCheck.Close();
+            string sSql = "INSERT INTO DangNhap (Ten_dang_nhap, Mat_khau) VALUES (N'" + name + "', N'" + password + "')";
+            mySqlCommand = new SqlCommand(sSql, mySqlConnection);
+            mySqlCommand.ExecuteNonQuery();
+            MessageBox.Show("Đăng ký thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            txtTendangky.Clear();
+            txtMatkhaudangky.Clear();
+            txtXacnhanmatkhau.Clear();
+        }
+        private void SignUpForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.ExitThread();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            OptionForm op = new OptionForm();
+            op.Show();
+            this.Hide();
+        }
+    }
+}
